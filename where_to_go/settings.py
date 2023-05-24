03a6d7106df8182468931bec718dc70cb7917b34
Arkadiy <capark74@mail.ru>
2023-05-24 20:07:22 +0300

    Настроены переменные окружения

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xg0=%k@7!km_$(8m!^16^^2xty@7br2bxknvxkdufp*_9n0#!3'
+INTERNAL_IPS = ["127.0.0.1",]
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
 
-ALLOWED_HOSTS = []
+SECRET_KEY = env.str('SECRET_KEY')
+
+DEBUG = env.bool('DEBUG')
 
 
 # Application definition
@@ -39,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'places.apps.PlacesConfig',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
