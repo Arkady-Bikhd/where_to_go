af6af82c9f505b30754fb1c5beda8f36c7ba5ea9
Arkadiy <capark74@mail.ru>
2023-07-14 21:23:34 +0300

    Загружена исправная версия

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -9,34 +9,41 @@ class Command(BaseCommand):
     help = 'Загрузка данных из файлов'
 
     def add_arguments(self, parser) -> None:
-        parser.add_argument('url', type=str, help='адрес файла данных в формате json')
+        parser.add_argument(
+            'url', type=str, help='адрес файла данных в формате json')
 
-    def handle(self, *args, **options) -> str | None:
+    def handle(self, *args, **options) -> None:
         json_file_url = options['url']
         response = requests.get(json_file_url)
         response.raise_for_status()
         place_params = response.json()
-        title = place_params['title']
-        description_short = place_params.get(['description_short'], '')
-        description_long = place_params.get(['description_long'], '')
-        latitude = place_params['coordinates']['lat']
-        longitude = place_params['coordinates']['lng']
-        image_urls = place_params.get(['imgs'], [])
-        place, created = Place.objects.get_or_create(
-            title=title,
-            defaults={
-                description_short: description_short,
-                description_long: description_long,
-                latitude: latitude,
-                longitude: longitude,
-            }
-        )
-        if created:
-            save_place_image(place, image_urls)
-            print(f'Объект {title} с соответствующими изображениями создан')
+        try:
+            title = place_params['title']
+            description_short = place_params.get('description_short', '')
+            description_long = place_params.get('description_long', '')
+            latitude = place_params['coordinates']['lat']
+            longitude = place_params['coordinates']['lng']
+            image_urls = place_params.get('imgs', [])
+            place, created = Place.objects.get_or_create(
+                title=title,
+                defaults={
+                    'description_short': description_short,
+                    'description_long': description_long,
+                    'latitude': latitude,
+                    'longitude': longitude,
+                }
+            )
+            if created:
+                save_place_image(place, image_urls, title)
+                print(
+                    f'Объект {title} с соответствующими изображениями создан')
+        except KeyError:
+            print('Отсутствуют данные в файле')
+        except Place.MultipleObjectsReturned:
+            print('В базе данных найдено несколько записей')
 
 
-def save_place_image(place, image_urls) -> None:
+def save_place_image(place, image_urls, title) -> None:
     for number, image_url in enumerate(image_urls):
         image_response = requests.get(image_url)
         image_response.raise_for_status()
